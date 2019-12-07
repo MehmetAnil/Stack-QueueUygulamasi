8e5184f52e39d6f9d0e6c75ca9bd8a7101b1259c
Mehmet Anıl TAYSİ <36363193+MehmetAnil@users.noreply.github.com>
2019-12-07 14:08:34 +0300

    Add files via upload

---
+++ Karınca.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje2A2
+{
+    class Karınca
+    {
+        private string ad;
+        private int can;
+
+        public Karınca(string ad)
+        {
+            this.ad = ad;
+        }
+        public Karınca(string ad, int can)
+        {
+            this.ad = ad;
+            this.can = can;
+        }
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public int Can
+        {
+            get { return can; }
+            set { can = value; }
+        }
+
+
+    }
+}
