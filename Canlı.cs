ea46cd38add336d2fbd87d7e3bfeb3d77e5c8e17
Mehmet Anıl TAYSİ <36363193+MehmetAnil@users.noreply.github.com>
2019-12-09 21:33:59 +0300

    Add files via upload

---
+++ Canlı.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje2A2
+{
+    class Canlı
+    {
+        private string ad;
+        private int can;
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        public int Can
+        {
+            get { return can; }
+            set { can = value; }
+        }
+
+
+    }
+}
